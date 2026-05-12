36d786da582b2703c850814fda9e36f569c4d9f6
Bilolbek Rayimov <rayimovbilolbek@yandex.com>
2026-05-12 15:06:15 +0200

    Initial Commit

---
+++ ManageIt/ManageItApp.swift
@@ -0,0 +1,17 @@
+//
+//  ManageItApp.swift
+//  ManageIt
+//
+//  Created by Bilolbek Rayimov on 12/05/26.
+//
+
+import SwiftUI
+
+@main
+struct ManageItApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
